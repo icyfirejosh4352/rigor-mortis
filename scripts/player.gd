99be330336b4a15c12946258b04d3121c19edaaa
icyfirejosh4352 <icyfirejosh4352@gmail.com>
2026-04-30 07:00:00 +0530

    Movement rework
    reworked movement, now state machine based. also changed some constants. Wallrunning yet to be implemented.

--- scripts/player.gd
+++ scripts/player.gd
@@ -28,9 +28,9 @@ const JUMP_SPDBST = 1.5
 # Smoothing for Accelaration, applied separately from GROUND_SLOW_MULT (I think?).
 const ACCEL_SMOOTH = 5.0
 # friction during Sliding. less than Ground, more than Air.
-const SLIDE_SLOW = 0.2
-# Initial speed boost that the velocity of the x and z axes are multiplied.
-const SLIDE_SPDBST = 2.0
+const SLIDE_SLOW = 0.4
+# Initial speed boost that the velocity of the x and z axes are multiplied by.
+const SLIDE_SPDBST = 3.0
 # minimum speed while sliding. could use this for crouching.
 const SLIDE_MINSPEED = 2.0
 # Wallrunning speed multiplier.
@@ -41,6 +41,14 @@ var IsSliding = false
 
 # mouse sensitivity.
 @export var mouse_sensitivity: float = 0.002
+
+enum PlayerStates
+{
+	DEFAULT,
+	SLIDING,
+	WALLRUNNING
+}
+var PlayerState = PlayerStates.DEFAULT
 
 # idk sum mouse shi
 func _unhandled_input(event: InputEvent) -> void:
@@ -58,111 +66,108 @@ func _unhandled_input(event: InputEvent) -> void:
 
 # All the physics calculations.
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+	
+	if Input.is_action_pressed("slide") && Input.is_action_pressed("jump"):
+		print("both pressed")
+	
+#	PlayerState = PlayerStates.DEFAULT
 	var input_dir := Input.get_vector("strafe left", "strafe right", "forward", "backward")
 	var direction := (eyes.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	#Air friction calculation. only occurs while in air. (I think)
 	if !is_on_floor():
+		velocity += get_gravity() * delta
 		velocity.x = lerp(velocity.x, direction.x * SPEED, ACCEL_SMOOTH * delta * (1/AIR_SLOW_MULT))
 		velocity.z = lerp(velocity.z, direction.z * SPEED, ACCEL_SMOOTH * delta * (1/AIR_SLOW_MULT))
-		IsSliding = false
-		
-		# Wallrunning.
-		# I'm sorry, I had to ask gemini for checking collsiding plane's normal.
-#		var collsion = move_and_collide(velocity * delta)
-#		if collsion:
-#			var normal = collsion.get_normal()
-#			var Ref = Vector3.UP
-#			var angle = rad_to_deg(normal.angle_to(Ref))
-#			print (angle)
-#			
-#			if angle >= 75 && angle <= 100:
-#				print ("Can Wallrun")
-			
-		
-	#All the important stuff is here.
-	elif is_on_floor():
-		# By default, assumes the player isn't sliding.
-		IsSliding = false
-		
-		# Multiplies velocity by SLIDE_SPDBST when the slide button is intially pressed.
-		if Input.is_action_just_pressed("slide"):
-			IsSliding = true
-			velocity = Vector3(velocity.x * SLIDE_SPDBST, velocity.y, velocity.z * SLIDE_SPDBST)
-			
-		# Lerps(I think) the velocity to the minimum sliding velocity.
-		if Input.is_action_pressed("slide"):
-			IsSliding = true
-			velocity = velocity.move_toward(Vector3(SLIDE_MINSPEED * direction.x, velocity.y, SLIDE_MINSPEED * direction.z), SLIDE_SLOW)
-			
-			# Disables the head collider.
-			# TODO: add a boolean for sliding (I dont wanna keep checking Input.is_action_pressed)
-			head.disabled = true
-		
-		# If not pressing Slide and there is any direction input at all, 
-		# sets velocity to direction into speed for x and z axes, using ACCEL_SMOOTH as delta.
-		elif direction.z != 0 && direction.x != 0:
-			velocity = velocity.move_toward(Vector3(direction.x * SPEED, direction.y, direction.z * SPEED), ACCEL_SMOOTH)
-			
-		# If no input in either axes (x or z), uses different method (just lerp) to calculate velocity.
-		# Definitely a better way to do this exists, but it's 1:00 and I wanna sleep.
-		# also think this messes with the previous elif clause's velocity calculation. possibly. I dont care, movement feels okay.
-		# TODO: fix ts 
-		else:
-			if direction.z == 0:
-				velocity.z = lerp(velocity.z, direction.z * SPEED, ACCEL_SMOOTH * delta * GROUND_SLOW_MULT)
-			if direction.x == 0:
-				velocity.x = lerp(velocity.x, direction.x * SPEED, ACCEL_SMOOTH * delta * GROUND_SLOW_MULT)
-				
-			# Enables the head collider.
-			head.disabled = false
-	
-	# Jumps if on floor.
-	# TODO: rework this for future possibility of double jump and wall jump, maybe use a boolean, idk.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		# Instantly sets y velocity to JUMP_VELOCITY. do I need to change this? idk
-		velocity.y = JUMP_VELOCITY
-		
-		#TODO: findout if this screws with the other calculation.
-		velocity.x += direction.x * (SPEED * JUMP_SPDBST)
-		velocity.z += direction.z * (SPEED * JUMP_SPDBST)
-		
-	if IsSliding:
-		FullMesh.visible = false
-		SlideMesh.visible = true
-	else:
+		PlayerState = PlayerStates.DEFAULT
+		head.disabled = false
 		FullMesh.visible = true
 		SlideMesh.visible = false
+
+	#All the important stuff is here.
+	elif is_on_floor():
+		PlayerState = PlayerStates.DEFAULT
+		if Input.is_action_just_pressed("slide"):
+			PlayerState = PlayerStates.SLIDING
+			velocity = Vector3(velocity.x * SLIDE_SPDBST, velocity.y, velocity.z * SLIDE_SPDBST)
+		elif Input.is_action_pressed("slide"):
+			PlayerState = PlayerStates.SLIDING
+			
+		match PlayerState:
+			PlayerStates.DEFAULT:
+				head.disabled = false
+				FullMesh.visible = true
+				SlideMesh.visible = false
+				
+							
+				# If not sliding and there is any direction input at all, 
+				# sets velocity to direction into speed for x and z axes, using ACCEL_SMOOTH as delta.
+				if direction.z != 0 && direction.x != 0:
+					velocity = velocity.move_toward(Vector3(direction.x * SPEED, direction.y, direction.z * SPEED), ACCEL_SMOOTH)
+
+				# If no input in either axes (x or z), uses different method (just lerp) to calculate velocity.
+				# Definitely a better way to do this exists, but it's 1:00 and I wanna sleep.
+				# also think this messes with the previous elif clause's velocity calculation. possibly. I dont care, movement feels okay.
+				# TODO: fix ts 
+				else:
+					if direction.z == 0:
+						velocity.z = lerp(velocity.z, direction.z * SPEED, ACCEL_SMOOTH * delta * GROUND_SLOW_MULT)
+					if direction.x == 0:
+						velocity.x = lerp(velocity.x, direction.x * SPEED, ACCEL_SMOOTH * delta * GROUND_SLOW_MULT)
+						
+				# TODO: rework this for future possibility of double jump and wall jump, maybe use a boolean, idk.
+				if Input.is_action_just_pressed("jump"):
+					# Instantly sets y velocity to JUMP_VELOCITY. do I need to change this? idk
+					velocity.y = JUMP_VELOCITY
+					
+					#TODO: findout if this screws with the other calculation.
+					velocity.x += direction.x * (SPEED * JUMP_SPDBST)
+					velocity.z += direction.z * (SPEED * JUMP_SPDBST)
+			PlayerStates.SLIDING:
+				velocity = velocity.move_toward(Vector3(SLIDE_MINSPEED * direction.x, velocity.y, SLIDE_MINSPEED * direction.z), SLIDE_SLOW)
+				FullMesh.visible = false
+				SlideMesh.visible = true
+				head.disabled = true
+				
+				# TODO: rework this for future possibility of double jump and wall jump, maybe use a boolean, idk.
+				if Input.is_action_just_pressed("jump"):
+					head.disabled = false
+					FullMesh.visible = true
+					SlideMesh.visible = false
+					PlayerState = PlayerStates.DEFAULT
+					# Instantly sets y velocity to JUMP_VELOCITY. do I need to change this? idk
+					velocity.y = JUMP_VELOCITY
+					
+					#TODO: findout if this screws with the other calculation.
+					velocity.x += direction.x * (SPEED * JUMP_SPDBST)
+					velocity.z += direction.z * (SPEED * JUMP_SPDBST)
+
 	
 	move_and_slide()
 	
-	if is_on_wall():
-		var wall_normal = get_wall_normal()
-		var wall_angle = rad_to_deg(wall_normal.angle_to(Vector3.UP))
+#	if is_on_wall():
+#		var wall_normal = get_wall_normal()
+#		var wall_angle = rad_to_deg(wall_normal.angle_to(Vector3.UP))
 		
 #		var flat_wallN = Vector3(wall_normal.x, 0, wall_normal.z).normalized()
 #		var flat_LookDir = Vector3(-global_transform.basis.z.x, 0, -global_transform.basis.z.z).normalized()
 		
 #		var angle_diff = flat_wallN.signed_angle_to(flat_LookDir, Vector3.UP)
-		var LookMult = 0
-		if input_dir.y > 0:
-			LookMult = -1
-		elif input_dir.y < 0:
-			LookMult = 1
+#		var LookMult = 0
+#		if input_dir.y > 0:
+#			LookMult = -1
+#		elif input_dir.y < 0:
+#			LookMult = 1
 #		print ("LookMult: ",LookMult)
 #		print ("wall_angle: ", wall_angle)
 #		print("angle_diff: ", angle_diff)
 #		print ("flat wallN: ", flat_wallN)
 #		print ("flat LookDir: ", flat_LookDir)
-		if wall_angle >= 75 && wall_angle <= 100:
+#		if wall_angle >= 75 && wall_angle <= 100:
 #			print ("can wallrun")
 #			print (wall_normal)
-			var rotatedNormal = wall_normal.rotated(Vector3.UP, deg_to_rad(90)).normalized()
-			velocity = rotatedNormal * SPEED * WALLRUN_MULT
-			print (velocity)
+#			var rotatedNormal = wall_normal.rotated(Vector3.UP, deg_to_rad(90)).normalized()
+#			velocity = rotatedNormal * SPEED * WALLRUN_MULT
+#			print (velocity)
 			
 			# need to rework ENTIRE movement system to use state machines. highly inefficient to do everything like this.
